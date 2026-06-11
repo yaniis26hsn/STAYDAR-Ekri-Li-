695f88fd453e0580e565e649fb30d2678682d00f
yaniishsn <hassainiyanis2006@gmail.com>
2026-06-11 12:13:13 +0100

    .

--- BACKEND/controllers/appartement.js
+++ BACKEND/controllers/appartement.js
@@ -10,6 +10,9 @@ export const getAppartements = async (req,res)=>{
 export const createAppartement = async (req, res) => {
   // here one important thing must be sent is the token , it is necessary so that 
 // we can get the userId 
+console.log(req) 
+
+
   const newAppart = new Appartement({
     ...req.body,
     ownerId: req.user.userId
